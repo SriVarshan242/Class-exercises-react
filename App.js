e3d023abd7e0e5a5a686b3f6a17a8aabb79fede1
SriVarshan242 <727723eucs242@skcet.ac.in>
2024-09-19 15:53:38 +0530

    Add files via upload
    app.js and app.css are uploaded for blood donation app

---
+++ App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import "./App.css";
+import Home from "./Components/Login Form/Home";
+import Login from "./Components/Login Form/Login";
+import Signup from "./Components/Login Form/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
