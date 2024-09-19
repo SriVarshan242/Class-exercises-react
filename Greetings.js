23caaa00980691327214058b51c8be1269ceb989
SriVarshan242 <727723eucs242@skcet.ac.in>
2024-09-19 16:21:07 +0530

    Delete Greetings.js

--- Greetings.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Greetings(props){
-    return(
-        <h1 style={{color:"blue",backgroundColor:"black",paddingRight:"40px"}}>Welcome,{props.name}!</h1>
-    )
-}
-export default Greetings;
